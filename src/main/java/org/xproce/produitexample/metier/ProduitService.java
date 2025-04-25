4bfdab78a92b0202e3e529f120a9dd641bf5124b
Xproce <badr.hirchoua@gmail.com>
2025-04-25 02:16:30 -0700

    Update and rename ProduitManager.java to ProduitService.java

--- src/main/java/org/xproce/produitexample/metier/ProduitService.java
+++ src/main/java/org/xproce/produitexample/metier/ProduitService.java
@@ -1,4 +1,4 @@
-package org.xproce.produitexample.metier;
+package org.xproce.produitexample.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
@@ -6,13 +6,22 @@ import org.xproce.produitexample.dao.entities.Produit;
 
 import java.util.List;
 
-@Component
-public interface ProduitManager {
+
+public interface ProduitService {
+    
     public Produit addProduit(Produit produit);
+
     public Page<Produit> getAllProduits(int page, int taille);
+
     public Page<Produit> searchProduits(String keyword, int page, int taille);
+
     public List<Produit> getByKeyword(String keyword);
+
     public Produit getProduitById(Integer id);
+
     public Produit updateProduit(Produit produit);
+
     public boolean deleteProduit(Integer id);
+
+    public List<Produit> getAllProduits();
 }
